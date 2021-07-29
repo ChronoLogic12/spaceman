c618940104dc25f71de3f033ef5e432443726cc9
ChronoLogic12 <msuttie12@hotmail.com>
2021-07-29 12:57:04 +0000

    Feat: Add feature to check if tile has already been tried and toggle the active/inactive styling of guessed tiles

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,21 @@
+//import array of words to guess
+import words from "./words.js";
+
 //global variables
 
 let word = "";
 let currentGuess = [];
-
-//import array of words to guess
-import words from "/assets/js/words.js";
+let prevGuesses = [];
 
 //select random word and set 
 
 let setTargetWord = () => {
     word = words[Math.floor(Math.random()*words.length)];
     word.split('').forEach(char => {
-        currentGuess.push("<span class='tile inactive'>_</span>");
+        currentGuess.push("<div class='tile inactive'>_</div>");
     });
     updateCurrentGuess();
+    subtractOneFromCountdown();
 }
 
 // update current guess
@@ -23,16 +25,29 @@ let updateCurrentGuess = () => {
     $(".target-word").append(`${currentGuess.join("")}`);
 }
 
+//update countdown
+
+let subtractOneFromCountdown = () => {
+    let currentCount = parseInt($(".counter").text());
+    $(".counter").text(--currentCount);
+}
+
 //check word contains selected letter
 
 let checkSelectedLetter = (event) => {
+    if (prevGuesses.includes(event.target.innerText)) {
+        return;
+    }
+    prevGuesses.push(event.target.innerText);
+    event.target.classList.toggle("active");
+    event.target.classList.toggle("inactive");
     for (let i=0; i< word.length; i++) {
-        console.log(word);
-        console.log(word[i].toUpperCase() === event.target.innerText);
-        console.log(currentGuess);
         if (word[i].toUpperCase() === event.target.innerText) {
-            currentGuess[i] = `<span class='tile inactive'>${word[i].toUpperCase()}</span>` ;
-        }     
+            currentGuess[i] = `<div class='tile active'>${word[i].toUpperCase()}</div>` ;
+        }
+    }
+    if (!word.toUpperCase().includes(event.target.innerText)) {
+        subtractOneFromCountdown();
     }
     updateCurrentGuess();
 }
